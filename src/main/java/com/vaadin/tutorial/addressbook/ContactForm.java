16fd714a2b88d81a77f4dbf7bcc282584b45f12e
Ndiforamang Fusi <fusisoft@gmail.com>
2024-05-22 06:54:56 -0500

    Added field for birth day

--- src/main/java/com/vaadin/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/vaadin/tutorial/addressbook/ContactForm.java
@@ -29,7 +29,7 @@ public class ContactForm extends FormLayout {
     TextField lastName = new TextField("Last name");
     TextField phone = new TextField("Phone");
     TextField email = new TextField("Email");
-    DateField birthDate = new DateField( "date");
+    DateField birthDate = new DateField( "Date of birth");
 
     Contact contact;
 
